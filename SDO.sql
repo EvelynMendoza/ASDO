02bac5cc6b6a9e4e58a4d04b90790aa3b5b9e684
OctavioLuis <oct.luisg@gmail.com>
2019-09-26 23:32:25 -0500

    Se realiza el CRUD para la tabla consumidores

--- SDO.sql
+++ SDO.sql
@@ -1,29 +1,39 @@
+drop DATABASE  if exists SDO;
 CREATE DATABASE if not exists SDO;
 use SDO;
 CREATE TABLE consumidores (
   idConsumidor int AUTO_INCREMENT,
-   nombreCompleto varchar(100),
-   direccion varchar(100),
-   telefono varchar(100),
-   numMedidor varchar(100),
-   primary key(idConsumidor)
+  numUsuario varchar(50),
+  numMedidor varchar(50),
+  nombreCompleto varchar(100),
+  direccion varchar(100),
+  manzana int,
+  telefono varchar(15),
+  primary key(idConsumidor)
    );
 
 CREATE TABLE consumo(
  idConsumo int AUTO_INCREMENT,
  idUsuario int,
- periodo varchar(20),
- fechaInicio varchar(20),
- fechaIfin varchar(20),
- mCubicos double,
- pConsumo double,
- pTotal double,
+ lecturaActual double,
+ consumoMedidor double,
+ precio double,
+ importeConsumo double,
+ coutaFija double,
+ recargos double,
+ cooperacion double,
+ bonificaciones double,
+ sanciones double,
+ varios double,
+ totalPagar double,
+ fechaPAgo date,
+ notas varchar(300),
+ avis varchar(300),
  status varchar(10),
 primary key(idConsumo)
 );
 
-select * from consumidores;
-select * from consumidores where idConsumidor=1 ;
-
-
 -- insert into consumidores(nombreCompleto, direccion, telefono,  numMedidor) values("octavio","Zaragosa 20","9515194019", 244);
+insert into consumidores(numUsuario, numMedidor, nombreCompleto,  direccion, manzana, telefono) 
+values("410","11028358","PEDRO GARCÍA LÓPEZ","PRIV. MORELOS#2",2,"951 128 00 98");
+
